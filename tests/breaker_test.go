03f1d77263bd7216688fd84fdf19f4b29778c0c3
Leandro Rabindranath Leon <leandro.leon@geniussports.com>
2025-02-26 15:01:50 -0500

    feat: Enhance breaker functionality and add tests
    This commit introduces several enhancements to the breaker
    functionality, including a Reset method for the LatencyWindow
    and comprehensive test cases to ensure proper operation under
    various conditions. Also added enable/disable functionality.
    
    The changes include:
    - Adding Reset function to LatencyWindow to properly reset it.
    - Adding tests to check that memory limit is triggered
    - Adding tests to check that latencies and memory limits are checked.
    - Adding Enable/Disable functionality to the breaker

--- tests/breaker_test.go
+++ tests/breaker_test.go
@@ -106,3 +106,107 @@ func Test_breaker_should_trigger_if_memory_is_above_threshold(t *testing.T) {
 
 	assert.True(t, b.Allow(), "Breaker should allow because of memory usage")
 }
+
+func Test_breaker_should_trigger_if_memory_is_above_threshold_and_latencies_are_above_threshold(t *testing.T) {
+
+	breaker.MemoryLimit = 512 * 1024 * 1024 // 512 MB
+
+	b := breaker.NewBreaker(&breaker.Config{
+		MemoryThreshold:   0.5,
+		LatencyThreshold:  600,
+		LatencyWindowSize: 10,
+		Percentile:        0.95,
+		WaitTime:          10,
+	})
+
+	// Add 10 latencies above the threshold and verify the breaker is triggered
+	for i := 0; i < 10; i++ {
+		val := 300 + i*50
+		latency := time.Duration(val) * time.Millisecond
+		now := time.Now()
+		startTime := now.Add(-latency)
+		endTime := now
+		b.Done(startTime, endTime)
+	}
+
+	assert.True(t, b.TriggeredByLatencies(), "Breaker should be triggered")
+
+	assert.False(t, b.Allow(), "Breaker should not allow")
+
+	// now we reduce MemoryLimit to trigger the breaker and reset the breaker
+	breaker.MemoryLimit = 1 * 1024 * 1024 // 256 MB
+
+	b.Reset()
+
+	assert.False(t, b.TriggeredByLatencies(), "Breaker should not be triggered due to memory usage")
+
+	assert.False(t, b.Allow(), "Breaker should not allow because of memory usage")
+
+	// now we increase MemoryLimit to allow the breaker
+	breaker.MemoryLimit = 512 * 1024 * 1024 // 512 MB
+
+	assert.True(t, b.Allow(), "Breaker should allow")
+
+	// Add 10 latencies under the threshold and verify the breaker is not triggered
+	for i := 0; i < 10; i++ {
+		// random latency between 100 and 500
+		val := rand.Int()%400 + 100
+		latency := time.Duration(val) * time.Millisecond
+		startTime := time.Now().Add(-latency)
+		endTime := time.Now()
+		b.Done(startTime, endTime)
+	}
+
+	assert.False(t, b.TriggeredByLatencies(), "Breaker should not be triggered")
+
+	assert.True(t, b.Allow(), "Breaker should allow")
+}
+
+func Test_Breaker_Enable_Disable(t *testing.T) {
+	b := breaker.NewBreaker(&breaker.Config{
+		MemoryThreshold:   0.5,
+		LatencyThreshold:  600,
+		LatencyWindowSize: 10,
+		Percentile:        0.95,
+		WaitTime:          10,
+	})
+
+	breaker.MemoryLimit = 512 * 1024 * 1024 // 512 MB
+
+	assert.True(t, b.Allow(), "Breaker should allow")
+	assert.True(t, b.IsEnabled(), "Breaker should be enabled")
+
+	// put some latencies below the threshold
+	for i := 0; i < 10; i++ {
+		// random latency between 100 and 500
+		val := rand.Int()%400 + 100
+		latency := time.Duration(val) * time.Millisecond
+		startTime := time.Now().Add(-latency)
+		endTime := time.Now()
+		b.Done(startTime, endTime)
+	}
+
+	assert.True(t, b.Allow(), "Breaker should allow")
+	assert.True(t, b.IsEnabled(), "Breaker should be enabled")
+
+	b.Disable()
+	assert.True(t, b.Allow(), "Breaker should allow when disabled")
+	assert.False(t, b.IsEnabled(), "Breaker should be disabled")
+
+	b.Enable()
+	assert.True(t, b.Allow(), "Breaker should allow when enabled")
+	assert.True(t, b.IsEnabled(), "Breaker should be enabled")
+
+	// put some latencies above the threshold
+	for i := 0; i < 10; i++ {
+		val := 300 + i*50
+		latency := time.Duration(val) * time.Millisecond
+		now := time.Now()
+		startTime := now.Add(-latency)
+		endTime := now
+		b.Done(startTime, endTime)
+	}
+
+	assert.False(t, b.Allow(), "Breaker should not allow when triggered")
+	assert.True(t, b.IsEnabled(), "Breaker should be enabled")
+}
